bced94c76ef44e6ff8321364514e192e2bed5f10
Camas <camas@hotmail.co.uk>
2019-12-11 22:04:49 +0000

    Add known answer tests to Q3

--- 3/solution.rs
+++ 3/solution.rs
@@ -1,7 +1,7 @@
 pub mod question3 {
     use std::collections::HashMap;
 
-    pub fn solve(data: Vec<String>) -> String {
+    pub fn solve(data: Vec<&str>) -> String {
         let path1 = travel(&data[0]);
         let path2 = travel(&data[1]);
 
@@ -68,5 +68,26 @@ pub mod question3 {
         }
 
         return output;
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::solve;
+
+        #[test]
+        fn test_known_answers() {
+            let answer = solve(vec!["R8,U5,L5,D3", "U7,R6,D4,L4"]);
+            assert_eq!(answer, "6 30");
+            let answer = solve(vec![
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83",
+            ]);
+            assert_eq!(answer, "159 610");
+            let answer = solve(vec![
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+            ]);
+            assert_eq!(answer, "135 410");
+        }
     }
 }
